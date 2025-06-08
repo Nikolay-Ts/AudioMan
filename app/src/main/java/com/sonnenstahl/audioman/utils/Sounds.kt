7b890692e70ec61bf4337c0dd0493e2902e05068
Nik Tsonev <nikolay.l.tsonev@gmail.com>
2025-06-08 16:23:15 +0200

    added sounds data class

---
+++ app/src/main/java/com/sonnenstahl/audioman/utils/Sounds.kt
@@ -0,0 +1,18 @@
+package com.sonnenstahl.audioman.utils
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Sounds(
+    var title: String,
+    var description: String,
+    var filePath: String,
+    var imagePath: String = "default.png"
+)
+
+val defaultSounds = listOf(
+    Sounds("Coffee Shop", "people talking in a coffee shop" , "coffee_shop"),
+    Sounds("Rain"       , "rain outside of your window"     , "rain"),
+    Sounds("Forest"     ,  "wild forest"                    , "forest"),
+    Sounds("Campfire"   , "sitting by the campfire"         , "fire")
+)
